65dec45da97834026c50d83a40fd4723419f89d9
Vesa Laakso <vesa.laakso@venuu.fi>
2014-05-04 23:46:45 +0300

    Fetch matches to proper ical format

--- script.rb
+++ script.rb
@@ -50,7 +50,57 @@ class SplUusimaa
   def login!
     raise LoginFailed unless login
   end
+
+  def matches
+    @matches ||= begin
+      raw_ical = fetch_matches_with_cache
+      tmp = ical_matches(raw_ical)
+      fix_descriptions(tmp)
+    end
+  end
+
+  def fetch_matches_with_cache
+    if File.exists?("tmp.ics")
+      File.read("tmp.ics")
+    else
+      fetched = fetch_matches
+      File.write("tmp.ics", fetched)
+      fetched
+    end
+  end
+
+  def fetch_matches
+    response = self.class.get('/tehtavakalenteri.php', query: {
+      tuomari: @referee_id
+    })
+
+    # spluusimaa.fi returns invalid iCal format, fix it to be importable.
+    raw_ical = response.body.gsub(%r{^DT(START|END);}, 'DT\1:')
+
+    # The encoding is said to be ISO-8859-1 but it is actually UTF-8
+    raw_ical.encode!('utf-8', 'utf-8')
+  end
+
+  def ical_matches(raw_ical)
+    RiCal.parse_string(raw_ical).first
+  end
+
+  def fix_descriptions(ical_data)
+    ical_data.events.each do |event|
+      descr = event.description.gsub(%r{<a.+href=.+otteluid=(\d+).+</a></a>}, '\1')
+      event.description = descr
+    end
+  end
 end
 
 client = SplUusimaa.new('account@example.com', 'password')
+
+puts 'Logging in...'
 client.login!
+puts 'Logged in!'
+
+puts 'Fetching matches...'
+matches = client.matches
+puts 'Matches fetched!'
+
+binding.pry
