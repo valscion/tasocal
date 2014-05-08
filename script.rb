1afcf644eb8dbddf0e90a1076c0f9f8d3d269f92
Vesa Laakso <vesa.laakso@venuu.fi>
2014-05-08 19:23:39 +0300

    Move event formatter to app directory, separate file

--- script.rb
+++ script.rb
@@ -6,41 +6,7 @@ require 'httparty'
 require 'pry'
 
 require './app/spl_uusimaa'
-
-class EventFormatter
-  attr_reader :ical
-
-  def initialize(ical_data)
-    @ical = ical_data
-  end
-
-  def remove_events_beginning_after(time)
-    @ical.events.reject! do |event|
-      event.dtstart < time
-    end
-  end
-
-  def set_event_lengths(length)
-    @ical.events.each do |event|
-      event.dtend = event.dtstart + length
-    end
-  end
-
-  def join_consecutive_events
-    grouped_events = @ical.events.group_by do |event|
-      s = event.dtstart
-      "#{s.year}-#{s.month}-#{s.day}"
-    end
-    grouped_events.each_value do |grouped_events|
-      sorted = grouped_events.sort {|a, b| b.dtstart <=> a.dtstart}
-      last_start = nil
-      sorted.each do |event|
-        event.dtend = last_start unless last_start.nil?
-        last_start = event.dtstart
-      end
-    end
-  end
-end
+require './app/event_formatter'
 
 client = SplUusimaa.new('account@example.com', 'password')
 
