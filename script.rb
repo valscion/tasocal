735772da220351ffb1885cdeb5651aa9577eb59a
Vesa Laakso <vesa.laakso@venuu.fi>
2014-05-08 18:57:27 +0300

    Implement joining of consecutive events

--- script.rb
+++ script.rb
@@ -26,6 +26,21 @@ class EventFormatter
       event.dtend = event.dtstart + length
     end
   end
+
+  def join_consecutive_events
+    grouped_events = @ical.events.group_by do |event|
+      s = event.dtstart
+      "#{s.year}-#{s.month}-#{s.day}"
+    end
+    grouped_events.each_value do |grouped_events|
+      sorted = grouped_events.sort {|a, b| b.dtstart <=> a.dtstart}
+      last_start = nil
+      sorted.each do |event|
+        event.dtend = last_start unless last_start.nil?
+        last_start = event.dtstart
+      end
+    end
+  end
 end
 
 client = SplUusimaa.new('account@example.com', 'password')
@@ -41,5 +56,6 @@ puts 'Matches fetched!'
 matches = EventFormatter.new(matches_raw)
 matches.remove_events_beginning_after(DateTime.now)
 matches.set_event_lengths(2.hours)
+matches.join_consecutive_events
 
 binding.pry
