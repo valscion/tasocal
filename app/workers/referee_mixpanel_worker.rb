d0ec3e9fd79f10b289bf5ebc68d3c25309efaca7
Vesa Laakso <vesa.laakso@venuu.fi>
2014-05-11 18:12:59 +0300

    Split refereemixpanelworker to methods

--- app/workers/referee_mixpanel_worker.rb
+++ app/workers/referee_mixpanel_worker.rb
@@ -8,36 +8,17 @@ class RefereeMixpanelWorker
     options = options.with_indifferent_access
     mixpanel_token = options[:mixpanel_token]
     mixpanel_options = options[:mixpanel_options]
-    people_options = mixpanel_options.slice(:distinct_id, :ip)
 
-    result = HTTParty.get("http://www.spluusimaa.fi/taso/ottelulista.php?tuomari=#{referee_id}")
-    regex_name = result.response.body.match(%r{<h1>([^<]+)</h1>}) if result.response.code == 200
-    optional_params = {}
-    unless regex_name.nil?
-      full_name = regex_name[1]
-      last_name, first_name = full_name.split(" ", 2)
-      if first_name.nil? || last_name.nil?
-        optional_params['$name'] = full_name
-      else
-        optional_params['$first_name'] = first_name
-        optional_params['$last_name'] = last_name
-      end
-    end
+    basic_options = mixpanel_options
+      .slice(:distinct_id, :ip)
+      .merge(optional_params: optional_params(mixpanel_options[:properties]))
 
-    time_ms = if mixpanel_options[:properties].present?
-      mseconds = mixpanel_options[:properties][:time].to_f * 1000.0
-      mseconds > 1 ? mseconds.to_i : nil
-    end
-    optional_params['$time'] = time_ms if time_ms
-
-    people_options.merge(optional_params: optional_params)
-
-    once_options = people_options.merge({
+    once_options = basic_options.merge({
       properties: {
         'First Calendar Feed Fetch' => Time.now,
-      }
+      }.merge(fetch_name_hash(referee_id))
     })
-    increment_options = people_options.merge({
+    increment_options = basic_options.merge({
       properties: {
         'Feed Results Fetched' => 1
       }
@@ -46,5 +27,34 @@ class RefereeMixpanelWorker
     MixpanelPeopleWorker.perform_async(mixpanel_token, :set_once, once_options)
     MixpanelPeopleWorker.perform_async(mixpanel_token, :increment, increment_options)
     MixpanelWorker.perform_async(mixpanel_token, mixpanel_options)
+  end
+
+  private
+
+  def fetch_name_hash(referee_id)
+    person = {}
+    result = HTTParty.get("http://www.spluusimaa.fi/taso/ottelulista.php?tuomari=#{referee_id}")
+    regex_name = result.response.body.match(%r{<h1>([^<]+)</h1>}) if result.response.code == 200
+    unless regex_name.nil?
+      full_name = regex_name[1]
+      last_name, first_name = full_name.split(" ", 2)
+      if first_name.nil? || last_name.nil?
+        person['$name'] = full_name
+      else
+        person['$first_name'] = first_name
+        person['$last_name'] = last_name
+      end
+    end
+    person
+  end
+
+  def optional_params(properties)
+    result = {}
+    time_ms = unless properties.nil?
+      mseconds = properties[:time].to_f * 1000.0
+      mseconds > 1 ? mseconds.to_i : nil
+    end
+    result['$time'] = time_ms if time_ms
+    result
   end
 end
